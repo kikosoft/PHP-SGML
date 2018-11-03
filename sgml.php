a52cd1f02619bd3cf9a6d9b5f6ba3bbc8cbca058
Peter de Jong <dejong@kikosoft.com>
2018-11-03 13:57:04 +0100

    Update sgml.php

--- sgml.php
+++ sgml.php
@@ -1,67 +1,115 @@
 <?php
 
-/*********************************************************************************
- **                                                                             **
- **   Standard Generalized Markup Language generator                            **
- **                                                                             **
- **       version 1.0    date 22-feb-2013                                       **
- **       version 2.0    date 30-oct-2018                                       **
- **                                                                             **
- **   This class defines how tag-valid SGML code can be generated from PHP.     **
- **   It is specifically, and only, made for generating SGML quickly.           **
- **                                                                             **
- *********************************************************************************/
+/**
+ * SGML - PHP SGML composer class.
+ * In SGML each element has a start tag, content, and an end tag.
+ * A start tag can have several attributes. Each attribute has a name and value.
+ *
+ * @see       https://github.com/kikosoft/SGML The SGML GitHub project
+ *
+ * @author    Peter de Jong <dejong@kikosoft.com>
+ * @copyright 2018 Peter de Jong
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GNU General Public License
+ * @note      This program is distributed in the hope that it will be useful - WITHOUT
+ *            ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *            FITNESS FOR A PARTICULAR PURPOSE.
+ */
 
-interface interface_SGML
+namespace html5php\html5;
+
+/**
+ * Low level SGML class.
+ */
+class SGML
 {
-  public function processArguments($arguments);
-  public function __construct($name,$arguments = NULL,$isVoid = FALSE);
-  public function minimize();
-  public function block();
-  public function unblock();
-  public function write($content);
-  public function attach($element);
-  public function attributes($attributes);
-  public function attribute($name,$value,$append = FALSE);
-  public function comment($comment);
-  public function getMarkup($minimize = TRUE,$indentLevel = 0);
-  public function flush($minimize = TRUE,$handle = NULL);
-}
+  /**
+   * Detemine how a void element is closed.
+   * Use '' for html, and ' /' for xml.
+   *
+   * @var string
+   */
+  private $voidClosure  = '';
 
-/***************************************************
- **  CONSTRUCTION                                 **
- ***************************************************/
+  /**
+   * Whether this element is without a closing tag, like: <br>.
+   *
+   * @var bool
+   */
+  private $isVoid       = FALSE;
 
-class SGML implements interface_SGML
-// an sgml element typically consists of three parts: a start tag, content, and an end tag.
-{
+  /**
+   * Whether the markup of this element will be minimized.
+   *
+   * @var bool
+   */
+  private $minimize     = FALSE;
 
-  private $isVoid       = FALSE;  // elements without a closing tag, like: <br>
-  private $minimize     = FALSE;  // markup of element will always be minimized
-  private $blocked      = FALSE;  // when blocked you cannot flush the end tag
-  private $startFlushed = FALSE;  // indicate that the start tag was flushed
-  private $name         = '';     // tag name, if absent no tag is outputted, only content
-  private $content      = '';     // is always a string, if present there are no child elements
-  private $elements     = [];     // refers to other sgml elements, if present there is no content
-  private $attributes   = [];     // key is attribute name, value is attribute value
+  /**
+   * Whether this element is locked so you cannot flush the end tag.
+   *
+   * @var bool
+   */
+  private $blocked      = FALSE;
 
+  /**
+   * Whether the start tag of this element was flushed.
+   *
+   * @var bool
+   */
+  private $startFlushed = FALSE;
 
+  /**
+   * The tag name of this element.
+   * If absent no start or end tag, only content, is send to the output.
+   *
+   * @var string
+   */
+  private $name         = '';
+
+  /**
+   * The content of this element, apart from child elements.
+   * If present there are no child elements.
+   *
+   * @var string
+   */
+  private $content      = '';
+
+  /**
+   * The child elements of this element.
+   * If present there is no content.
+   *
+   * @var array
+   */
+  private $elements     = [];
+
+  /**
+   * Attributes of this element.
+   * The array keys are attribute names, and array values are attribute values.
+   *
+   * @var array
+   */
+  private $attributes   = [];
+
+  /**
+   * Split arguments into content and attributes.
+   *
+   * Example 1: 'my content'
+   * Example 2: ['size' => 5, 'title' => 'test]
+   * Example 3: ['my content',['size' => 5, 'title' => 'test]]
+   * Example 4: [['size' => 5, 'title' => 'test],'my content']
+   * Example 5: [['size' => 5],'my content',['title' => 'test],'more content']
+   *
+   * @param array $arguments Arguments supplied to a SGML element
+   * @return array|null Array containing content and attributes, or NULL.
+   */
   public function processArguments($arguments)
-  // split arguments into content and attributes. if an argument is a string
-  // then it is content. if arguments is an array then it represents an attributes.
-  // example 1: 'my content'
-  // example 2: ['size' => 5, 'title' => 'test]
-  // example 3: ['my content',['size' => 5, 'title' => 'test]]
-  // example 4: [['size' => 5, 'title' => 'test],'my content']
-  // example 5: [['size' => 5],'my content',['title' => 'test]]
   {
-    // start with nothing
+    // start with no content or attributes
     $content    = '';
     $attributes = [];
     // any arguments?
     if (isset($arguments))
     {
-      // if it is an array then we need to look deeper
       if (is_array($arguments))
       foreach ($arguments as $key => $value)
       {
@@ -78,81 +126,104 @@ class SGML implements interface_SGML
       // a non-array is always seen as content
       else $content = $arguments;
     }
-    // return an array with content and attributes
     return [$content,$attributes];
   }
 
-
-  public function __construct($name,$arguments = NULL,$isVoid = FALSE)
-  // create an element with a name and optionally some content
+  /**
+   * Create an element with a name and optionally some content.
+   *
+   * @param string $name
+   * @param array|null $arguments
+   */
+  public function __construct($name,$arguments = NULL)
   {
     // split arguments into content and attributes
     list($content,$attributes) = $this->processArguments($arguments);
     // store
     $this->name    = $name;
     $this->content = $content;
-    $this->isVoid  = $isVoid;
     // attach attributes
     if (count($attributes) > 0) $this->attributes($attributes);
-    // the constructor always returns the object created
   }
 
-/***************************************************
- **  MINIMIZE                                     **
- ***************************************************/
-
+  /**
+   * Render the inside of this element minimized.
+   *
+   * @return object
+   */
   public function minimize()
-  // render the inside of this element minimized
   {
     $this->minimize = TRUE;
     // return for chaining
     return $this;
   }
 
-/***************************************************
- **  BLOCKING                                     **
- ***************************************************/
+  /**
+   * This is a void, or self closing, element.
+   * SGML doesn't really have void elements, but html and xml do.
+   *
+   * @param string $closure Use either ' /' or '', the latter is the default.
+   * @return object
+   */
+  public function void($closure = '')
+  {
+    $this->voidClosure = $closure;
+    $this->isVoid      = TRUE;
+    // return for chaining
+    return $this;
+  }
 
+  /**
+   * When blocked you cannot flush the end tag.
+   *
+   * @return object
+   */
   public function block()
-  // when blocked you cannot flush the end tag
   {
     $this->blocked = TRUE;
     // return for chaining
     return $this;
   }
 
-
+  /**
+   * When blocked you cannot flush the end tag.
+   *
+   * @return object
+   */
   public function unblock()
-  // when blocked you cannot flush the end tag
   {
     $this->blocked = FALSE;
     // return for chaining
     return $this;
   }
 
-/***************************************************
- **  NAME                                         **
- ***************************************************/
-
+  /**
+   * Does this element have a name?
+   *
+   * @return bool
+   */
   private function _hasName()
-  // does this element have a name?
   {
     return ($this->name != '');
   }
 
-/***************************************************
- **  CONTENT                                      **
- ***************************************************/
-
+  /**
+   * Does this element have content?
+   *
+   * @return bool
+   */
   private function _hasContent()
-  // does this element have content?
   {
     return ($this->content != '');
   }
 
-
+  /**
+   * Add some content to this element.
+   * @param string $content Add content to the sgml element
+   *
+   * @return object
+   */
   public function write($content)
-  // add some content to this element
   {
     // if elements are present we should add the content as an element
     if ($this->_hasElements()) $this->_attachNew('',$content);
@@ -164,33 +235,42 @@ class SGML implements interface_SGML
     return $this;
   }
 
-/***************************************************
- **  ELEMENTS                                     **
- ***************************************************/
-
+  /**
+   * Does this element have one or more child elements?
+   *
+   * @return bool
+   */
   private function _hasElements()
-  // does this element have one or more child elements?
   {
     return count($this->elements) > 0;
   }
 
-
-  private function _new($name,$arguments = NULL,$isVoid = FALSE)
-  // create a new element with a name and optionally some content
+  /**
+   * Create a new element with a name and optionally some content.
+   *
+   * @param string $name
+   * @param array|null $arguments
+   * @return object
+   */
+  private function _new($name,$arguments = NULL)
   {
     // return a new element
-    return new SGML($name,$arguments,$isVoid);
+    return new SGML($name,$arguments);
   }
 
-
+  /**
+   * Add a new element to this one.
+   *
+   * @param object $element
+   * @return object
+   */
   public function attach($element)
-  // add a new element to this one
   {
     // if a content is present we should promote it to an element
     if ($this->_hasContent())
     {
       // create a new element with the current content
-      $this->elements[] = new SGML('',$this->content);
+      $this->elements[] = $this->_new('',$this->content);
       // and clear the content
       $this->content = '';
     }
@@ -198,43 +278,63 @@ class SGML implements interface_SGML
     return $this->elements[] = $element;
   }
 
-
-  private function _attachNew($name,$arguments = NULL,$isVoid = FALSE)
-  // create a new element and attach it to the current
+  /**
+   * Ccreate a new element and attach it to the current.
+   *
+   * @param string $name
+   * @param array|null $arguments
+   * @return object
+   */
+  private function _attachNew($name,$arguments = NULL)
   {
-     return $this->attach($this->_new($name,$arguments,$isVoid));
+     return $this->attach($this->_new($name,$arguments));
   }
 
-
+  /**
+   * This magic function creates a new element with any name, content and attributes.
+   *
+   * @param string $name
+   * @param array|null $arguments
+   * @return object
+   */
   public function __call($name,$arguments)
-  // this magic function creates a new element with any name, content and attributes
   {
     // make new element
     return $this->_attachNew($name,$arguments);
   }
 
-/***************************************************
- **  ATTRIBUTES                                   **
- ***************************************************/
-
+  /**
+   * Returns all one attribute, if it exists, otherwise we return an empty string.
+   *
+   * @param string $name
+   * @return string
+   */
   private function _getAttribute($name)
-  // returns all one attribute, if it exists, otherwise we return an empty string
   {
     return isset($this->attributes[$name]) ? $this->attributes[$name] : '';
   }
 
-
+  /**
+   * Set attributes, always supply an associative array of attributes.
+   *
+   * @param array|null $attributes
+   * @return object
+   */
   public function attributes($attributes)
-  // set attributes, always supply an associative array of attributes
   {
     foreach ($attributes as $name => $value) $this->attribute($name,$value);
-    // return for chaining
     return $this;
   }
 
-
+  /**
+   * Set an attribute, overwrite it when it exists.
+   *
+   * @param string $name
+   * @param string $value
+   * @param bool $append
+   * @return object
+   */
   public function attribute($name,$value,$append = FALSE)
-  // set an attribute, overwrite it when it exists
   {
     // append value to existing value
     if ($append) $value = trim($this->_getAttribute($name).' '.$value);
@@ -244,24 +344,25 @@ class SGML implements interface_SGML
     return $this;
   }
 
-/***************************************************
- **  COMMENTS                                     **
- ***************************************************/
-
+  /**
+   * Add a comment content, a comment is always an element with the name '--'.
+   *
+   * @param string $comment
+   * @return object
+   */
   public function comment($comment)
-  // add a comment content, a comment is always an element with the name '--'
   {
     $this->_attachNew('--',$comment);
     // return for chaining
     return $this;
   }
 
-/***************************************************
- **  MARKUP                                       **
- ***************************************************/
-
+  /**
+   * Returns the start tag with attributes.
+   *
+   * @return string
+   */
   private function _startTag()
-  // give back the start tag with attributes
   {
     // no start when it has already been flushed
     if ($this->startFlushed) return '';
@@ -270,22 +371,32 @@ class SGML implements interface_SGML
     // then the list of attributes
     foreach ($this->attributes as $attribute => $value)
     {
-      $text .= ' '.(($value == '') ? $attribute : $attribute.'="'.addslashes($value).'"');
+      // if the attribute is numeric it is a boolean attributes
+      if (is_numeric($attribute)) $text .= ' '.addslashes($value);
+      else $text .= ' '.(($value == '') ? $attribute : $attribute.'="'.addslashes($value).'"');
     }
-    // and return it as a tag
-    return '<'.$text.'>';
+    // and return it as a tag, also sets correct closure
+    return '<'.$text.($this->isVoid ? $this->voidClosure : '').'>';
   }
 
-
+  /**
+   * Returns the end tag.
+   *
+   * @return string
+   */
   private function _endTag()
-  // give back the end tag
   {
     return ($this->isVoid || $this->blocked) ? '' : '</'.$this->name.'>';
   }
 
-
+  /**
+   * Produces the sgml output string.
+   *
+   * @param bool $minimize
+   * @param int $indentLevel
+   * @return string
+   */
   public function getMarkup($minimize = TRUE,$indentLevel = 0)
-  // produces the sgml output string
   {
     // this is the normal indent string for this element
     $indent = str_repeat('  ',$indentLevel);
@@ -321,12 +432,14 @@ class SGML implements interface_SGML
     return $minimize ? $sgml : $indent.$sgml.PHP_EOL;
   }
 
-/***************************************************
- **  FLUSHING                                     **
- ***************************************************/
 
+  /**
+   * Flush markup to the given file handle.
+   *
+   * @param bool $minimize
+   * @param object|null $handle
+   */
   public function flush($minimize = TRUE,$handle = NULL)
-  // flush markup to the given file handle
   {
     // get the markup
     $markup = $this->getMarkup($minimize);
